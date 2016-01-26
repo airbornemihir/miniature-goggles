08853cecc8ac0895d002007d30d57411426c23b1
Mihir Mehta <mihirthegenius@gmail.com>
2016-01-26 10:22:46 -0600

    Added legality check to theorem.

--- something.lisp
+++ something.lisp
@@ -1,10 +1,10 @@
 (defun extract-scheme-from-char-list (char-list backwards-scheme)
-  (if (endp char-list) (mv char-list (REVERSE backwards-scheme) t) ;; error!
+  (if (endp char-list) (mv char-list (REVERSE backwards-scheme) "That's all? Your URL is just a scheme!") ;; error!
     (if (equal (car char-list) #\:)
 	(if (or
 	     (or (endp (cdr char-list)) (not (equal (cadr char-list) #\/)))
 	     (or (endp (cddr char-list)) (not (equal (caddr char-list) #\/)))) ;; error!
-	    (mv char-list (REVERSE backwards-scheme) t)
+	    (mv char-list (REVERSE backwards-scheme) "Your scheme is not followed by ://")
 	  (mv (cdddr char-list) (REVERSE backwards-scheme) nil))
       (extract-scheme-from-char-list
        (cdr char-list)
@@ -46,27 +46,30 @@
   (mv-let (a b c)
 	  (extract-scheme-from-char-list (coerce url 'LIST) nil)
 	  (if (equal c nil)
-	      (mv-let (d e)
-		      (extract-host-from-char-list a nil)
-		      (mv-let (f g)
-			      (extract-path-from-char-list d nil)
-			      (mv-let (h i)
-				      (extract-query-from-char-list f nil)
-				      (list
-				       (cons :scheme (coerce b 'STRING))
-				       (cons :host (coerce e 'STRING))
-				       (cons :path (coerce g 'STRING))
-				       (cons :query (coerce i 'STRING))
-				       (cons :fragment (coerce h 'STRING)))
-				      )
-			      ))
-	    (list (cons :error "That's all? Your URL is just a scheme!"))
+	      (if (legal-scheme-check b)
+		  (mv-let (d e)
+			  (extract-host-from-char-list a nil)
+			  (mv-let (f g)
+				  (extract-path-from-char-list d nil)
+				  (mv-let (h i)
+					  (extract-query-from-char-list f nil)
+					  (list
+					   (cons :scheme (coerce b 'STRING))
+					   (cons :host (coerce e 'STRING))
+					   (cons :path (coerce g 'STRING))
+					   (cons :query (coerce i 'STRING))
+					   (cons :fragment (coerce h 'STRING)))
+					  )
+				  ))
+		(list (cons :error "Illegal scheme.")))
+	    (list (cons :error c))
 	    )
 	  ))
 
 (defthm trouble-with-scheme
   (implies
    (mv-let (a b c) (extract-scheme-from-char-list (coerce url 'LIST) nil)
-	   (declare (ignore a) (ignore b)) (equal c t))
+	   (declare (ignore a))
+	   (or (not (equal c nil)) (not (legal-scheme-check b)) ))
    (equal (caar (parse-url url)) :error)
    ))
