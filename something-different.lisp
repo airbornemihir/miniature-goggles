a031d5c69622c3b8e5fcd1893e0b63cbe43d7382
Mihir Mehta <mihirthegenius@gmail.com>
2016-05-26 20:10:48 -0500

    We have a theorem that's got no counter-examples... but we can't prove it!

--- something-different.lisp
+++ something-different.lisp
@@ -1,6 +1,6 @@
 (include-book "std/lists/top" :dir :system)
 (include-book "acl2s/cgen/top" :dir :system :ttags :all)
-(acl2s-defaults :set testing-enabled t)
+(acl2s-defaults :set testing-enabled nil)
 
 (defund consume-separator (char-list separator)
   (if (endp separator)
@@ -59,14 +59,21 @@
     ;;   (not b)))
     ))
 
+;; (thm
+;;  (implies
+;;   (true-listp char-list)
+;;   (mv-let (a b c) (consume-through-separator char-list (car separators) nil)
+;;     (declare (ignore b) (ignore c)) (true-listp a))
+;;   ))
+
 (in-theory (disable consume-through-separator))
 
-(defund separate-char-list (char-list separator-list field-list)
+(defund separate-char-list (char-list separator-list field-separator-list)
   (declare (xargs :measure (len separator-list)))
   ;;endp requires true-listp
   (if (endp separator-list)
       ;; no more separators!
-      (reverse (cons char-list field-list))
+      (reverse (cons char-list field-separator-list))
     ;;still some separators
     (mv-let (a b c) (consume-through-separator char-list
 					       (car separator-list)
@@ -74,77 +81,87 @@
 	(declare)
       (if c
 	  ;; the separator was not found
-	  (reverse (cons char-list field-list))
+	  (reverse (cons char-list field-separator-list))
 	;; the separator was found, so let's recurse
 	(separate-char-list a
 			    (cdr separator-list)
-			    (cons b field-list))))))
+			    (cons (car separator-list) (cons b field-separator-list)))))))
 
-(defthm no-separator-one-field
-  (implies (not (consp separator-list))
-	   (equal (len (separate-char-list char-list separator-list field-list))
-		  (+ 1 (len field-list))))
-  :hints (("Goal" :in-theory (enable separate-char-list))))
+;; (defthm no-separator-one-field
+;;   (implies (not (consp separator-list))
+;; 	   (equal (len (separate-char-list char-list separator-list field-list))
+;; 		  (+ 1 (len field-list))))
+;;   :hints (("Goal" :in-theory (enable separate-char-list))))
 
 (in-theory (enable separate-char-list))
 
-(defthm separators-between-fields
-  (<=
-   (len (separate-char-list char-list separator-list field-list))
-   (+ 1 (len field-list) (len separator-list)))
-  ;; :hints (("Goal"
-  ;; 	   :induct (and
-  ;; 		    ;; (len field-list)
-  ;; 		    (len separator-list)))
-  ;; 	  ("Subgoal *1/1"
-  ;; 	   :in-theory (enable separate-char-list)))
-  )
+;; (defthm separators-between-fields
+;;   (<=
+;;    (len (separate-char-list char-list separator-list field-list))
+;;    (+ 1 (len field-list) (len separator-list)))
+;;   ;; :hints (("Goal"
+;;   ;; 	   :induct (and
+;;   ;; 		    ;; (len field-list)
+;;   ;; 		    (len separator-list)))
+;;   ;; 	  ("Subgoal *1/1"
+;;   ;; 	   :in-theory (enable separate-char-list)))
+;;   )
 
-(defun unseparate-char-list (separator-list field-list)
-  (if (endp field-list)
+(defun unseparate-char-list (field-separator-list)
+  (if (endp field-separator-list)
       ;;no more fields. this can happen even when there are yet
       ;;separators.
       nil
     (append
-     (car field-list)
-     (if (endp separator-list)
-	 nil
-       (append
-	(car separator-list)
-	(unseparate-char-list (cdr separator-list) (cdr field-list)))))))
+     (car field-separator-list)
+     (unseparate-char-list (cdr field-separator-list)))))
 
 (defun is-separator-list (separators)
     (cond
-     ((consp separators) (character-listp (car separators)))
-     (t (equal separators nil))))
+     ((atom separators) (equal separators nil))
+     (t (and
+         (character-listp (car separators))
+         (is-separator-list (cdr separators))))))
 
-(thm
- (mv-let (a b c) (consume-through-separator char-list (car separators) nil)
-   (declare) 
-   (implies
-    (and
-     (consp separators)
-     (not c)
-     (not (true-listp a))
-     (true-listp char-list)
-     (character-listp (car separators)))
-    (equal
-     (unseparate-char-list
-      separators
-      (separate-char-list a (cdr separators) (list b)))
-     char-list))))
+;; (thm
+;;  (mv-let (a b c) (consume-through-separator char-list (car separators) nil)
+;;    (declare) 
+;;    (implies
+;;     (and
+;;      (consp separators)
+;;      (not c)
+;;      (not (true-listp a))
+;;      (true-listp char-list)
+;;      (character-listp (car separators)))
+;;     (equal
+;;      (unseparate-char-list
+;;       separators
+;;       (separate-char-list a (cdr separators) (list b)))
+;;      char-list))))
 
-;; thanks to cgen, i get it
-;; this theorem is messed up in the event that the first separator doesn't even
-;; appear in the list
+(defthm consume-separator-gigo
+  (implies (character-listp char-list)
+	   (mv-let (a b) (consume-separator char-list separator)
+             (declare (ignore b))
+             (character-listp a)))
+  :hints (("Goal" :in-theory (enable consume-separator))))
+
+(defthm
+  consume-through-separator-gigo
+  (implies (and (character-listp char-list) (character-listp backwards-field))
+           (mv-let (a b c)
+             (consume-through-separator char-list separator backwards-field)
+             (declare (ignore b) (ignore c))
+             (character-listp a)))
+  :hints (("Goal" :in-theory (enable consume-through-separator))))
+
 (defthm unseparate-separate
   (implies (and
-            (true-listp char-list)
+            (character-listp char-list)
             (is-separator-list separators)
             )
 	   (equal
 	    (unseparate-char-list
-	     separators
 	     (separate-char-list
 	      char-list
 	      separators
